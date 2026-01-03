142a26c2ba516e964cb5761d3f75460c07f396b6
joshw105j <135837823+joshw105j@users.noreply.github.com>
2026-01-03 17:40:54 +0000

    Orginal challenge complete

--- src/01_01/ordinal_challenge.c
+++ src/01_01/ordinal_challenge.c
@@ -3,9 +3,28 @@
 char *ordinal(int v)
 {
 	/*
-	   - create this function
-	   - this code won't run with out it! i am testing something
+	 the function needs to return the appropriate suffix for the given integer v
 	 */
+	static char ordinal_suffix[3];
+
+	if (v % 10 == 1)
+	{
+		sprintf(ordinal_suffix, "st");
+	}
+	else if (v % 10 == 2)
+	{
+		sprintf(ordinal_suffix, "nd");
+	}
+	else if (v % 10 == 3)
+	{
+		sprintf(ordinal_suffix, "rd");
+	}
+	else
+	{
+		sprintf(ordinal_suffix, "th");
+	}
+
+	return ordinal_suffix;
 }
 
 int main()
@@ -13,16 +32,17 @@ int main()
 	int c;
 
 	/* output a table of 100 numbers */
-	for( c=1; c<=20; c++ )
+	for (c = 1; c <= 20; c++)
 	{
 		printf("%3d%s\t%3d%s\t%3d%s\t%3d%s\t%3d%s\n",
-				c, ordinal(c),
-				c+20, ordinal(c+20),
-				c+40, ordinal(c+40),
-				c+60, ordinal(c+60),
-				c+80, ordinal(c+80)
-			  );
+					 c, ordinal(c),
+					 c + 20, ordinal(c + 20),
+					 c + 40, ordinal(c + 40),
+					 c + 60, ordinal(c + 60),
+					 c + 80, ordinal(c + 80));
 	}
 
-	return(0);
+	printf("\n this is actually my code");
+
+	return (0);
 }
